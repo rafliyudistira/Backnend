062a65e72c9e2bb3f042be223a86f338d6fca8ee
unknown <afysaidz@gmail.com>
2022-12-16 15:30:10 +0700

    Beckend

---
+++ src/main/java/com/example/demo/repository/IuserRepo.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import com.example.demo.model.usermodel;
+
+public interface IuserRepo {
+	public usermodel insertUser(usermodel user);
+	public List<usermodel> getAlluser();
+	public usermodel updateUser (long id, usermodel user);
+	public usermodel deleteUser (long id);
+}
+
---
+++ src/main/java/com/example/demo/service/IuserService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.model.usermodel;
+public interface IuserService {
+	public usermodel insertUser(usermodel user);
+	public List<usermodel> getAlluser();
+	public usermodel updateUser (long id, usermodel user);
+	public usermodel deleteUser (long id);
+}
